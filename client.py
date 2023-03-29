c92770fbc6ac913801d6cb9dfafefc4cb779d770
Pumpkinjam <rnjsl1212@naver.com>
2023-03-29 23:08:53 +0900

    first commit

---
+++ client.py
@@ -0,0 +1,46 @@
+from socket import *
+import os
+import pickle
+
+
+def print_log(chat_log : dict) -> None:
+    for timestamp, content in chat_log.items():
+        ip, message = content
+        print(f'{ip} [{timestamp}] : {message}')
+
+
+serverName = input('input server IP : ')
+serverPort = int(input('input server Port : '))
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+
+clientSocket.send('\n\n'.encode())
+
+if clientSocket.recv(1024).decode() == 'OK':
+    print('connected.')
+else:
+    print('connection error.')
+    raise Exception
+
+
+try:
+    while True:
+        msg = input('\n\n>>> ')
+
+        clientSocket.send(msg.encode())
+        new_chat_log = pickle.loads(clientSocket.recv(1024))
+
+        os.system('cls' if '\\' in os.path else 'clear')
+        print_log(new_chat_log)
+
+except KeyboardInterrupt:
+    clientSocket.send('\t\n'.encode())
+    clientSocket.close()
+    print('disconnected.')
+    '''
+except:
+    clientSocket.send('\t\n'.encode())
+    clientSocket.close()
+    print('an error occurred.')
+'''
---
+++ server.py
@@ -0,0 +1,41 @@
+from socket import *
+import time
+import pickle
+
+serverPort = 1200
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+
+# ip_dict records ip and the latest timestamp
+ip_dict = dict()
+
+# dictionary of chat_log follows the following format :
+# { Timestamp : (ip, message) }
+chat_log = dict()
+
+print('Server Init OK')
+
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    payload = connectionSocket.recv(1024)
+    payload = payload.decode()
+
+    print(f'Connection from {addr[0]}')
+    print(f'  payload : "{payload}"')
+
+    if payload == '\n\n':       # payload only having two lf means the new participant
+        connectionSocket.send('OK'.encode())
+        ip_dict[addr[0]] = time.time()
+        print(f'{addr[0]} joined the chat.')
+        print(f'current : {ip_dict}')
+    elif payload == '\t\n':     # payload with \t\b means the participant's quit
+        print(f'{addr[0]} has left the chat.')
+        del ip_dict[addr[0]]
+        print(f'current : {ip_dict}')
+    else:                       # this is just a normal chatting message
+        ip_dict[addr] = timestamp = time.time()
+        chat_log[timestamp] = (addr[0], payload)
+        connectionSocket.send(pickle.dumps(chat_log))
+    connectionSocket.close()
+    
